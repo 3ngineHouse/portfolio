1fa5407818ed15a857e3b6bcc54c60d86e40d17c
3ngineHouse <james.e.house@hotmail.co.uk>
2021-03-10 17:03:04 +0000

    got canvases scaling correctly, and javascript to draw test shapes on them

---
+++ main.js
@@ -0,0 +1,16 @@
+function drawTransitions(){
+    var canvases = document.getElementsByClassName("transition");
+    console.log(canvases.length);
+
+    for(let i=0; i<canvases.length; i++){
+        var ctx = canvases[i].getContext('2d');
+        
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(canvases[i].width, 0);
+        ctx.lineTo(0, canvases[i].height);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
